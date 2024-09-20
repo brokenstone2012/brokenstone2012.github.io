6ef441ff643e75eac5627e734ff8d4745b922e6e
brokenstone2012 <brokenstone2012@gmail.com>
2024-09-19 19:09:32 -0700

    Site updated: 2024-09-19 19:09:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/19/linux笔记01/","2024/09/19/pug语法/","2024/09/19/hello-word/"];function toRandomPost(){
+var posts=["2024/09/19/中式教育/","2024/09/19/linux笔记01/","2024/09/19/pug语法/","2024/09/19/hello-word/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
